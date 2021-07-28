400d58fc9934c43522a745990a0e1facec1552b0
Trevor Maré <trevorm336@gmail.com>
2021-07-28 15:20:48 +0200

    Canvas Drawing Operations

--- src/Doodle.Abstractions/JsInterop/IJsInteropCanvas.cs
+++ src/Doodle.Abstractions/JsInterop/IJsInteropCanvas.cs
@@ -1,12 +1,40 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace Doodle.Abstractions.JsInterop
 {
+
+    public delegate void OnCanvasUpdatedHandler(object sender, List<Abstractions.Models.CanvasPath> commands);
+
     public interface IJsInteropCanvas
     {
+        event OnCanvasUpdatedHandler CanvasCommandsUpdated;
+
         Task InitialiseCanvas(ElementReference forElement);
+
+        Task SetBrushColor(string color);
+
+        Task SetBrushSize(int size);
+
+        Task Destroy();
+
+        Task Clear(bool clearHistory);
+
+        Task Refresh();
+
+        Task Restore(string commandJson);
+
+        ValueTask<bool> Undo();
+        
+        ValueTask<bool> Redo();
+
+        ValueTask<bool> CanUndo();
+
+        ValueTask<bool> CanRedo();
+
+        Task OnCanvasUpdated(string commandJson);
     }
 }
