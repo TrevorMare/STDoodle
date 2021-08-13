803c3283f05fef8ee8ad99d1c9a44f9822745e48
Trevor Mare <trevorm336dev@gmail.com>
2021-08-13 07:39:07 +0200

    Background Picker Component

---
+++ src/Doodle/Components/Toolbar/BackgroundPicker/BackgroundPickerComponent.razor.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Doodle.Components.Toolbar.BackgroundPicker
+{
+
+    public partial class BackgroundPickerComponent
+    {
+
+        #region Properties
+        [Parameter]
+        public bool IsOpen { get; set; }
+        #endregion
+
+        
+    }
+
+
+}
