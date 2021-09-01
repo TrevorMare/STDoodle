bc463f93dcf15ad95af72dd7f77d55dcdd490b60
Trevor Maré <trevorm336@gmail.com>
2021-09-01 08:45:30 +0200

    Save & Restore Functionality

--- src/Doodle.Abstractions/Interfaces/IDoodleSaveHandler.cs
+++ src/Doodle.Abstractions/Interfaces/IDoodleSaveHandler.cs
@@ -1,9 +1,13 @@
+using System.Threading.Tasks;
+
 namespace Doodle.Abstractions.Interfaces
 {
 
     public interface IDoodleSaveHandler
     {
         
+        Task<string> SaveDoodleDraw(IDoodleDrawInteraction doodleDrawInteraction);
+
     }
 
 }
--- src/Doodle/Helpers/DoodleSaveHandler.cs
+++ src/Doodle/Helpers/DoodleSaveHandler.cs
@@ -1,9 +1,25 @@
+using System.Threading.Tasks;
+using Doodle.Abstractions.Interfaces;
+
 namespace Doodle.Helpers
 {
 
+    public delegate void OnSaveDoodleDrawHandler(object sender, string jsonData);
+
     public class DoodleSaveHandler : Abstractions.Interfaces.IDoodleSaveHandler
     {
+        public event OnSaveDoodleDrawHandler OnSaveDoodleDraw;
 
+        public async Task<string> SaveDoodleDraw(IDoodleDrawInteraction doodleDrawInteraction)
+        {
+            string result = null;
+            if (doodleDrawInteraction?.DoodleStateManager != null)
+            {
+                result = await doodleDrawInteraction.DoodleStateManager.SaveCurrentState();
+                this.OnSaveDoodleDraw?.Invoke(this, result);
+            }
+            return result;
+        }
 
     }
 
