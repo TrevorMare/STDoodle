9ea76881a30e8325544cae9ed7ed18532978920e
Trevor Maré <trevorm336@gmail.com>
2021-09-21 15:02:51 +0200

    Doodle Demo Pages

--- src/STDoodle.DemoWASM/Program.cs
+++ src/STDoodle.DemoWASM/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using GTour;
 
 namespace STDoodle.DemoWASM
 {
@@ -19,6 +20,9 @@ namespace STDoodle.DemoWASM
       builder.RootComponents.Add<App>("#app");
 
       builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      
+      builder.Services.UseGTour();
+      
       builder.Services.UseDoodle((config) => 
       {
         config.DefaultStrokeColor = "#FF0000";
